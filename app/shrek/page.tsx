aa5ebfbc6a1c30ee2aaeda56da4f0522174ee113
Enzo Calleya <enzouz12@gmail.com>
2023-12-08 05:10:34 +0100

    feat:shrekIA TextBox & ReplyBox

--- app/shrek/page.tsx
+++ app/shrek/page.tsx
@@ -1,10 +1,16 @@
+import ChatbotInput from '@/components/chat-inut/chat-input'
 import styles from './page.module.css'
 import Image from 'next/image'
-
 export default function Shrek() {
+
+    const handleSendMessage = (message: any) => {
+        // À ce stade, vous pouvez envoyer le message au chatbot ou effectuer toute autre action nécessaire
+        console.log('Message envoyé au chatbot:', message);
+    };
+
     return (
         <main className={styles.main}>
-            <div>
+            <div className={styles.center}>
                 <div className={styles.img}>
                     <Image
                         width={500}
@@ -19,6 +25,15 @@ export default function Shrek() {
                         alt="shrek bouche"
                     />
                 </div>
+                <div className={styles.styledTextContainer}>
+                    <p className={styles.styledText}>
+                        Bienvenue dans le monde stylisé ! Ce texte est stylisé avec des propriétés CSS.
+                    </p>
+                    
+                </div>
+                <div>
+                        <ChatbotInput />
+                    </div>
             </div>
         </main>
     )
