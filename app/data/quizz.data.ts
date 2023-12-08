3098677c1a8e2f66717953982a5f775c965dd1a3
p2104031 <corentin.claudel@etu.univ-lyon1.fr>
2023-12-08 03:23:50 +0100

    Rajout de phrase pour le Quizz

--- app/data/quizz.data.ts
+++ app/data/quizz.data.ts
@@ -1,6 +1,14 @@
 import { QuizzQuestionType } from '../types/quizz/quizz.type';
 
 export const q: QuizzQuestionType[] = [
-    {question: 'Croyez-vous au réchauffement climatique ?',answers : ['Bien sur !','Non, c\'est un complt des chinois'],correctAnswer:'Connard'},
-    {question: 'Aimez-vous avez très très très chaud ?', answers : ['Oui, Je suis un bandeur d\'été','Non, je suis un bandeur d\'hiver'], correctAnswer:'Oui, Je suis un bandeur d\'hiver'}
+    {question: 'Est ce que le réchauffement climatique est réel ?',answers:['Oui','Non c\'est une pur invention des scientifiques chinois'],correctAnswer:'Oui'},
+    {question: 'Le changement climatique est-il dû au cycle naturel de la Terre ?',answers : ['La nature est plus grande que nous','L\'activité humaine à un impact significatif'],correctAnswer:'L\'activité humaine à un impact significatif'},
+    {question: 'Les hiver étant froid, le réchauffement climatique n\'existe donc pas ?', answers : ['Le climat est quelque chose de longue période','L\'hiver peut avoir des variations'], correctAnswer:'Le climat est quelque chose de longue période'},
+    {question: 'Les émissions de CO2 sont-ils responsables du réchauffement climatique ?', answers: ['Oui car ce sont des gaz à effet de serre','Les sandwich du Crous en sont le vrai problème'],correctAnswer:'Oui car ce sont des gaz à effet de serre'},
+    {question: 'Nous ne sommes pas tous d\'accord sur ce qui se passe', answers: ['La TV ne nous donne jamais la même réponse','La grande majorité des scientifiques sont d\'accord'], correctAnswer:'La grande majorité des scientifiques sont d\'accord'},
+    {question: 'Le réchauffement climatique est bon pour certaine région. Le climat est plus doux',answers:['Ils ont beaucoup de chance, leurs conditions de vie va s\'améliorer','Cela va détruire la faune et la flore'],correctAnswer:'Cela va détruire la faune et la flore'},
+    {question: '"Ecoutez, moi il y a les mêmes températures depuis 50 ans"',answers:['Le ressenti d\'une personne ne représente pas la réalité','Ecoutons cette personnes rempli de sagesse de par son age'],correctAnswer:'Le ressenti d\'une personne ne représente pas la réalité'},
+    {question: '',answers:['',''], correctAnswer:''},
+    // {question: '',answers:['',''], correctAnswer:''},
+
 ];
